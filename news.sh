f52e70c6a46c40b9f84a8aad46c32bfd1ac8ca92
Sama Yanick <95623410+sama0074@users.noreply.github.com>
2023-04-22 01:43:05 +0100

    Update news.sh

--- news.sh
+++ news.sh
@@ -2,3 +2,15 @@ echo "Hello world"
 echo "Enter you first name"
 read name
 echo "Welcome to Cameroon $name"
+echo "Whats your gender"
+read sex
+if [ $sex == male ]
+then
+echo "$name is a male"
+elif [ $sex == female ]
+then
+echo "How come $name is a female"
+else
+echo "Oboy you no get gender?"
+fi
+
